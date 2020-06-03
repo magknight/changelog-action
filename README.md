6bfb62b9c3e6c2c928c76168050df9da218a2293
Alex Beavil <alex@aeroniemi.com>
2020-06-03 20:07:23 +0100

    v1

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # changelog-action
- Action to deal with our changelog style
+Action to deal with our changelog style
+
+## Usage
+```yaml
+      - name: Fiddle with changelog stuff
+        uses: ./tools/
+        id: changelog
+        with:
+          inputFile: ./release/aviatorsEdition150/changelog.md
+```
+## Outputs
+### Changes
+- ``changes``
+- The not-pretty version - just makes it look like what you see in the changelog.md file
+```md
+## 1.5.1
+- Updated credits page
+- GPWS flaps/gear messages
+- PA volume made a bit louder, and persistent
+- Better keyboard entry for TCPs
+- ND range knob direction (#376)
+- Updated SAM support to 1.0.7 (#302, #330)
+```
+### Pretty changes
+- ``prettyChanges``
+- The pretty version, with bugs turned into markdown links
+<!-- ```md
+## 1.5.1
+- Updated credits page
+- GPWS flaps/gear messages
+- PA volume made a bit louder, and persistent
+- Better keyboard entry for TCPs
+- ND range knob direction (#376)
+- Updated SAM support to 1.0.7 (#302, #330)
+``` -->
---
+++ changelog.js
@@ -0,0 +1,27 @@
+// const fs = require("fs")
+
+// try {
+//     var changelog = fs.readFileSync("./changelog.md",
+//         { encoding: 'utf8', flag: 'r' });
+//     // console.log(changelog)
+
+//     var latestStuff = changelog.match(/^(## [\s\S]+?)(?=\n## )/g).join("")
+//     var pretty = latestStuff.replace(/\(#(\d+)\)/g, `([#$1](https://magknight.org/bug/$1))`).replace(/\(#(\d*?), (#\d*?)\)/g, `([#$1](https://magknight.org/bug/$1), [#$2](https://magknight.org/bug/$2))`)
+//     console.log(pretty)
+// } catch (error) {
+//     core.setFailed(error.message);
+// }
+
+const core = require('@actions/core');
+const fs = require("fs")
+
+try {
+    var changelog = fs.readFileSync(core.getInput('inputFile'),
+        { encoding: 'utf8', flag: 'r' });
+    var latestStuff = changelog.match(/^(## [\s\S]+?)(?=\n## )/g).join("")
+    var pretty = latestStuff.replace(/\(#(\d+)\)/g, `([#$1](https://magknight.org/bug/$1))`).replace(/\(#(\d*?), (#\d*?)\)/g, `([#$1](https://magknight.org/bug/$1), [#$2](https://magknight.org/bug/$2))`)
+    core.setOutput("changes", latestStuff);
+    core.setOutput("prettyChanges", pretty);
+} catch (error) {
+    core.setFailed(error.message);
+}
