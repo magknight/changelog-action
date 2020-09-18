ece97ed24a8107e1f5a751243ab981de017930dc
Alex Beavil <alex@aeroniemi.com>
2020-09-18 16:31:01 +0100

    think i might've fixed the multiline issue i was having

--- changelog.js
+++ changelog.js
@@ -16,10 +16,12 @@ const core = require('@actions/core');
 const fs = require("fs")
 
 try {
-    var changelog = fs.readFileSync(core.getInput('inputFile'),
-        { encoding: 'utf8', flag: 'r' });
+    var changelog = fs.readFileSync(core.getInput('inputFile'), {
+        encoding: 'utf8',
+        flag: 'r'
+    });
     var latestStuff = changelog.match(/^(## [\s\S]+?)(?=\n## )/g).join("")
-    var pretty = latestStuff.replace(/\(#(\d+)\)/g, `([#$1](https://magknight.org/bug/$1))`).replace(/\(#(\d*?), (#\d*?)\)/g, `([#$1](https://magknight.org/bug/$1), [#$2](https://magknight.org/bug/$2))`)
+    var pretty = latestStuff.replace(/\(#(\d+)\)/g, `([#$1](https://magknight.org/bug/$1))`).replace(/\(#(\d*?), (#\d*?)\)/g, `([#$1](https://magknight.org/bug/$1), [#$2](https://magknight.org/bug/$2))`).replace(/\n/g, /\\n/)
     core.setOutput("changes", latestStuff);
     core.setOutput("prettyChanges", pretty);
 } catch (error) {
